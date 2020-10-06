5b7ff19c952571e4ce70f77857a449ba0d4cc9fb
Piero Cascio <piero.cascio@gmail.com>
2020-10-06 14:27:00 +0200

    Forth exercise - Jasmine Matchers toMatch, toEqual

--- jasmine/evaluation/specs/jasmine-matchers.spec.js
+++ jasmine/evaluation/specs/jasmine-matchers.spec.js
@@ -6,7 +6,7 @@
  * https://developer.mozilla.org/enUS/docs/Web/JavaScript/Guide/Regular_Expressions
  */
 describe("Jasmine Matchers", function () {
-//Scenario -1
+  //Scenario -1
   it("'toMatch' matcher should be applied successfully for regular expressions", function () {
     const strString1 = "The quick Brown Fox jumps over the lazy dog";
     const strPhone = "001-789-56-67";
@@ -16,4 +16,32 @@ describe("Jasmine Matchers", function () {
     fail('Use toMatch to check that the word JavaScript is not present');
     fail('Use toMatch to verify that the phone number has the expected format');
   });
+
+  //Scenario - 2
+  // describe("toEqual matcher should be applied successfully", function()
+  // {
+  //   it("if numbers are equal", function() {
+  //     const intVar = 15;
+  //     fail('Use the toEqual matcher to verify the intVar variable')
+  //   });
+  //   it("if strings are equal", function() {
+  //     const strVar = "Jasmine Cookbook";
+  //     fail('Use the toEqual matcher to verify the strVar variable')
+  //   });
+  //   it("if objects are equal", function() {
+  //     const myObectj1 = {a: 12, b: 13};
+  //     const myObectj2 = {a: 12, b: 13};
+  //
+  //     fail('Use the toEqual matcher to verify that myObectj1 is equal to myObectj2')
+  //     fail('Use the toEqual matcher to verify that myObectj1.a is equal to myObectj2.a')
+  //     fail('Use the toEqual matcher to verify that myObectj1.a is not equal to myObectj2.b')
+  //   });
+  //   it("if arrays are equal", function() {
+  //     const arr1 = [8, 9, 10];
+  //     fail('Use the toEqual matcher to verify the previous array')
+  //
+  //     const arr2 = [8, 9, 10, 11];
+  //     fail('Use the toEqual matcher to verify that the previous array is not equal to [8, 9, 10]')
+  //   });
+  // });
 });
