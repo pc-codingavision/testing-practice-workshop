d080c5259b7a196f464b66097db10bb5ed32092e
Piero Cascio <piero.cascio@gmail.com>
2020-10-08 16:41:21 +0200

    Add more test cases for the jasmine spies practice.

--- src/app/module/features/user/service/gateway-user.service.spec.ts
+++ src/app/module/features/user/service/gateway-user.service.spec.ts
@@ -1,12 +1,15 @@
 import { GatewayUserService } from './gateway-user.service';
 import { fail } from 'assert';
+import { mockUsers } from '../data/mock-data';
+import * as _ from 'lodash';
+import { UserRole } from '../model/User';
 
 /**
  * Implement the following test cases by using the TestBed Angular facility and a spy
  * for the related dependency.
  */
 xdescribe('GatewayUserService', () => {
-  let service: GatewayUserService;
+  let gatewayUserService: GatewayUserService;
 
   describe('#findUserById', () => {
     it('#findUserById should return user for existing id', () => {
@@ -16,37 +19,80 @@ xdescribe('GatewayUserService', () => {
       fail('Method not implemented, yet');
     });
     it('should call the UserService#findUserById', () => {
-      fail('Method not implemented, yet')
+      fail('Method not implemented, yet');
     });
     it('should call the UserService#findUserById only once', () => {
-      fail('Method not implemented, yet')
+      fail('Method not implemented, yet');
     });
     it('should call the UserService#findUserById with the passed id', () => {
-      fail('Method not implemented, yet')
+      fail('Method not implemented, yet');
     });
     it('should return empty when the UserService#findUserById returns an empty Observable', () => {
-      fail('Method not implemented, yet')
+      fail('Method not implemented, yet');
     });
   });
   describe('#findAllUsers', () => {
     it('#findAllUsers should return all users', () => {
       fail('Method not implemented, yet');
     });
+    it('should return an empty array when UserService#findAllUsers return an Observable of an empty array', () => {
+      fail('Method not implemented, yet');
+    });
+    it('should call UserService#findAllUsers', () => {
+      fail('Method not implemented, yet');
+    });
+    it('should call UserService#findAllUsers less than two times', () => {
+      fail('Method not implemented, yet');
+    });
+    it('should call UserService#findAllUsers with no parameters', () => {
+      fail('Method not implemented, yet');
+    });
   });
   describe('#findAllUsersByUserName', () => {
     it('#findAllUsersByUserName should return user for existing username', () => {
-      fail('Method not implemented, yet');
+      gatewayUserService.findAllUsersByUserName('Username2')
+        .subscribe(users => expect(users[0]).toEqual(
+          _.pick({
+            id: 2,
+            name: 'Name 2',
+            surname: 'Surname 2',
+            username: 'Username2',
+            role: UserRole.GOLD
+          }, ['id', 'username', 'role'])
+        ));
     });
     it('#findAllUsersByUserName should return empty for not existing username', () => {
+      gatewayUserService.findAllUsersByUserName('Username20')
+        .subscribe(users => expect(users.length).toEqual(0));
+    });
+    it('should call UserService#findAllUsersByUserName only once', () => {
       fail('Method not implemented, yet');
     });
   });
   describe('#findAllUsersByRole', () => {
     it('#findAllUsersByRole should return user for specified role', () => {
-      fail('Method not implemented, yet');
+      gatewayUserService.findAllUsersByRole(UserRole.GOLD)
+        .subscribe(users => expect(users[0]).toEqual(
+          _.pick({
+            id: 2,
+            name: 'Name 2',
+            surname: 'Surname 2',
+            username: 'Username2',
+            role: UserRole.GOLD
+          }, ['id', 'username', 'role'])
+        ));
     });
     it('#findAllUsersByRole should return empty for specified role', () => {
+      gatewayUserService.findAllUsersByRole(UserRole.VIP)
+        .subscribe(users => expect(users.length).toEqual(0));
+
+    });
+    it('should call UserService#findAllUsersByRole', () => {
       fail('Method not implemented, yet');
     });
   });
+
+  // ****************************************//
+  // ADD MORE TESTS IF YOU WISH TO DO SO ;) //
+  // ***************************************//
 });
