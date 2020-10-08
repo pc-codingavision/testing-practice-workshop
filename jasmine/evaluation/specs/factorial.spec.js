039da43a04eb9dc1139577fadf889f3883e6396b
Carmelo <melonemark8@gmail.com>
2020-10-08 11:47:29 +0200

    Seconda parte

--- jasmine/evaluation/specs/factorial.spec.js
+++ jasmine/evaluation/specs/factorial.spec.js
@@ -2,11 +2,11 @@ const factorial = require('../src/factorial')
 
 describe("Factorial", function () {
   it("should get factorial of given number", function () {
-    // Write your implementation here
-    fail('Method not implemented, yet!')
+    expect(factorial(2)).toBe(2)
   });
   it("should return null value for passing negative number or less/more than one argument", function () {
-    // Write your implementation here
-    fail('Method not implemented, yet!')
+    expect(factorial(-1)).toBeNull()
+    expect(factorial(2,2)).toBeNull()
+    expect(factorial()).toBeNull()
   });
 });
--- jasmine/evaluation/src/factorial.js
+++ jasmine/evaluation/src/factorial.js
@@ -1,3 +1,14 @@
 function factorial(num) {
-  // Write your implementation here
+if ( arguments.length != 1 || num < 0) {return null}
+
+var result = 1;
+
+for (i=num;i>1;i--) {
+  result = result * i;
 }
+
+return result;
+
+}
+
+module.exports = factorial;
